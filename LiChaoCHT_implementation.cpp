43c71c8597d524736822581a475989538dbf5e55
amunduzbaev <64077895+amunduzbaev@users.noreply.github.com>
2022-11-23 20:08:23 +0600

    Update LiChaoCHT_implementation.cpp

--- LiChaoCHT_implementation.cpp
+++ LiChaoCHT_implementation.cpp
@@ -1,52 +1,58 @@
-#define int long long
-struct node{
-	int m, c;
+const int N = 1e5 + 5;
+const int M = 1e9 + 5;
+
+struct line{
+	int m, b;
 	
-	int v(int x){
-		return (m * x + c);
+	int operator * (int x){
+		return m * x + b;
 	}
 	
-	int in(node b){
-		return floor((double)(b.c - c) / (double)(m - b.m));
-	}
+	int operator ^ (line x){
+		return ceil((b - x.b) / (x.m - m));
+	};
 };
 
-struct LiChaoST{
-	vector<node> tree;
-	vector<int> qq;
-	int n;
-	
-	void init(int sz, vector<int>& q){
-		n = sz;
-		tree.resize(n * 4, {(int)1e9, (int)1e9});
-		qq = q;
-		sort(qq.begin(), qq.end());
+struct LiChao{
+	line tree[N * 30];
+	int last, L[N * 30], R[N * 30];
+	LiChao(){
+		last = 0;
+		//~ for(int i=0;i<(N*30);i++) tree[i] = {M, M * M};
 	}
 	
-	void add(node v, int lx, int rx, int x){
-		if(lx == rx) { tree[x] = v; return; }
-		if(tree[x].m == v.m){
-			if(tree[x].m > v.m) tree[x] = v;
+	void add(line v, int lx, int rx, int x){
+		if(lx == rx){
+			if(tree[x] * lx > v * lx) swap(tree[x], v);
 			return;
 		}
-		int m = (lx + rx)>>1;
-		int in = v.in(tree[x]);
-		if(in < qq[m+1]){
-			if(tree[x].v(qq[m+1]) > v.v(qq[m+1])) swap(tree[x], v);
-			add(v, lx, m, x<<1);
-		} else {
-			if(tree[x].v(qq[m]) > v.v(qq[m])) swap(tree[x], v);
-			add(v, m+1, rx, x<<1|1);
+		if(v.m == tree[x].m){
+			tree[x].b = min(tree[x].b, v.b);
+			return;
 		}
-	} void add(node v){
-		add(v, 0, n - 1, 1);
+		
+		int m = (lx + rx) >> 1;
+		int ix = tree[x] ^ v;
+		if(ix <= m){
+			if(tree[x] * (m + 1) > v * (m + 1)) swap(tree[x], v);
+			add(v, lx, m, (L[x] ? L[x] : L[x] = ++last));
+		} else {
+			if(tree[x] * m > v * m) swap(tree[x], v);
+			add(v, m + 1, rx, (R[x] ? R[x] : R[x] = ++last));
+		}
+	} void add(lint v){
+		add(v, 0, M, 0);
 	}
 	
 	int get(int i, int lx, int rx, int x){
-		if(lx == rx) return tree[x].v(qq[i]);
-		int m = (lx + rx)>>1;
-		if(i <= m) return min(get(i, lx, m, x<<1), tree[x].v(qq[i]));
-		else return min(get(i, m+1, rx, x<<1|1), tree[x].v(qq[i]));
-	} int get(int i) { return get(i, 0, n - 1, 1); }
-}; 
+		if(lx == rx) return tree[x] * i;
+		int m = (lx + rx) >> 1;
+		if(i <= m && L[x]) return min(tree[x] * i, get(i, lx, m, L[x]));
+		if(m < i && R[x]) return min(tree[x] * i, get(i, m + 1, rx, R[x]));
+		return tree[x] * i;
+	} int get(int i){
+		return get(i, 0, M, 0);
+	}
+}tree;
 
+change max / min
