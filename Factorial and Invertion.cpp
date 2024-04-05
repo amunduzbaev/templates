3d29f5b6b251f210783736e9e995ca6b564f50e3
amunduzbaev <64077895+amunduzbaev@users.noreply.github.com>
2024-04-06 00:17:13 +0900

    Update Factorial and Invertion.cpp

--- Factorial and Invertion.cpp
+++ Factorial and Invertion.cpp
@@ -1,20 +1,27 @@
-void FINV(vector<int>& f, vector<int>& inv){
-    auto bp = [&](int a, int b){
-		int rr = 1;
+//~ define mod
+const int mod = 1e9 + 7;
+
+
+	auto bp = [&](int a, int b){
+		int c = 1;
 		while(b){
-			if(b&1) rr = rr * 1ll * a % mod;
+			if(b&1) c = c * 1ll * a % mod;
 			a = a * 1ll * a % mod, b >>= 1;
 		} return rr;
-    };
-    
-    f[0] = inv[0] = 1;
-    for(int i=1;i<(int)f.size();i++) f[i] = f[i-1] * 1ll * i % mod, inv[i] = bp(f[i], mod - 2);
-}
+	};
 
-//~ in low
+	auto f_and_inv = [&](vector<int>& f, vector<int>& inv){
+	    f[0] = inv[0] = 1;
+	    for(int i = 1;i < (int)f.size(); i++){
+			f[i] = f[i-1] * 1ll * i % mod;
+			inv[i] = bp(f[i], mod - 2);
+		}
+	};
 
-    vector<int> f(n + 1), inv(n + 1);
-    FINV(f, inv);
+	//~ initialize f and inv
+    vector<int> f(SZ), inv(SZ);
+    f_and_inv(f, inv);
     auto C = [&](int n, int k){
-      return f[n] * 1ll * inv[k] % mod * 1ll * inv[n - k] % mod;
+		if(n < k || k < 0) return 0ll;
+    	return f[n] * 1ll * inv[k] % mod * 1ll * inv[n - k] % mod;
     };
