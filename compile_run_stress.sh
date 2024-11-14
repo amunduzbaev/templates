7e97803dda70a9def4db31ec5c93457e7c65e82a
amunduzbaev <64077895+amunduzbaev@users.noreply.github.com>
2024-11-14 16:13:10 +0900

    Update compile_run_and_stress.sh

--- compile_run_stress.sh
+++ compile_run_stress.sh
@@ -1,32 +1,34 @@
-# compile and run
+# compile and run name.cpp with input in
 # if input empty, print empty in
+code=$1
+in=$2
 set -e
-g++ a.cpp -o run
+g++ $code.cpp -o run
 
 if [ ! -z "$(cat in)" ]; then 
-  ./run < in > out
+  ./run < $in > out
   echo "output:"
   cat out
   echo -e
 else 
-  echo "empyt in"
+  echo "empyt input file"
 fi
 
 #stres test
 #need files: 
-#           fast.cpp (solution you want to stres, prints wrong answer)
-#           slow.cpp (correct solution, to get the correct answer)
+#           wrong.cpp (solution you want to stres, prints wrong answer)
+#           corr.cpp (correct solution, to get the correct answer)
 #           gen.cpp (input test generator)
 #           no checker for now
 
 set -e
-g++ fast.cpp -o fast
+g++ wrong.cpp -o wrong
 g++ gen.cpp -o gen
 g++ slow.cpp -o slow
 for((i = 1; ; ++i)); do
     ./gen $i > input_file
-    ./fast < input_file > myAnswer
-    ./slow < input_file > correctAnswer
+    ./wrong < input_file > wrongAnswer
+    ./corr < input_file > correctAnswer
     diff -Z myAnswer correctAnswer > /dev/null || break
     echo "Passed test: "  $i
 done
